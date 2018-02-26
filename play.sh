2d95c8cf8f59adb497294e80ce9bc11e745b5017
Shola Emmanuel <shola.emmanuel@KaiTechs-MBP.domain.name>
2018-02-26 17:53:54 +0100

    updated saferm

--- play.sh
+++ play.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 #playing with codes before applying them
 
-itemsInDirectories=$(ls -l "$1" | sort -k1,1 | awk -F " " '{print $NF}' | sed -e '$ d')
-
 userReplyYes(){
     responseFirstLetter=$( echo "$1" | head -c 1)
     if [[ $responseFirstLetter == 'y' || $responseFirstLetter == 'Y' ]];
@@ -11,53 +9,62 @@ userReplyYes(){
     else
         false
     fi
+
 }
 
-examineSubDirectory(){
+recursivelyExamineSubDirectory(){
 
+currentDir=$1
+itemsInDirectories=$(ls -l "$currentDir" | sort -k1,1 | awk -F " " '{print $NF}' | sed -e '$ d')
 
 for item in $itemsInDirectories; do
-numberOfContentsInDirectory=$(ls -l "$1/$item" | sort -k1,1 | awk -F " " '{print $NF}' | sed -e '$ d' | wc -l | xargs)
+numberOfContentsInDirectory=$(ls -l "$currentDir/$item" | sort -k1,1 | awk -F " " '{print $NF}' | sed -e '$ d' | wc -l | xargs)
 
     #for directories
-    if [[ -d "$1/$item" ]]; then
-        read -p "examine files in $1/$item? " response
+    if [[ -d "$currentDir/$item" ]]; then
+        read -p "examine files in $currentDir/$item? " response
         userReplyYes $response
         #examine contents in subdirectory
-
             if [[ ($? -eq 0) ]] && [[ $numberOfContentsInDirectory -eq 0 ]]; then
-                read -p "remove $1/$item? " response
+                read -p "remove $currentDir/$item? " response
                 userReplyYes $response
                     if [[ ($? -eq 0) ]]; then
-                        echo "$1/$item removed"
+                        echo "$currentDir/$item removed"
                     else
-                        echo "$1/$item not removed"
+                        echo "$currentDir/$item not removed"
                     fi
             fi
+            userReplyYes $response
             if [[ ($? -eq 0) ]] && [[ $numberOfContentsInDirectory -gt 0 ]]; then
-                    #[[ ($? -eq 0) ]]; then
-                echo "$1/$item not empty"
-                read -p "examine files in $1/$item" response
-                userReplyYes $response
-                    if [[ ($? -eq 0) ]]; then
-                        examineSubDirectory $1/$item
-                    else
-                        echo "not examined"
-                    fi
+                    recursivelyExamineSubDirectory $currentDir/$item
+
             fi
+
+
     else
-        read -p "remove $1/$item? " response
+        read -p "remove $currentDir/$item? " response
         userReplyYes $response
             if [[ ($? -eq 0) ]]; then
-                echo "$1/$item removed"
+                echo "$currentDir/$item removed"
             else
-                echo "$1/$item not removed"
+                echo "$currentDir/$item not removed"
             fi
     fi
 done
 
+read -p "remove $currentDir? " response
+userReplyYes $response
+if [[ ($? -eq 0) ]]
+then
+    echo "$currentDir removed"
+else
+    echo "$currentDir not removed"
+fi
+
+currentDir=$(dirname $currentDir)
+
 }
 
-examineSubDirectory $1
+recursivelyExamineSubDirectory $1
 
 
